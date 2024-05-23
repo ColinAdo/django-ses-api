7f9b0c96b9bb5d8cec53c10469b8c5c403c3ae22
ColinAdo <colinadodev30@gmail.com>
2024-05-23 20:58:32 +0300

    First commit

---
+++ accounts/api/views.py
@@ -0,0 +1,15 @@
+from django.core.mail import send_mail
+from django.conf import settings
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class SendMailView(APIView):
+    def get(self, request, *args, **kwargs):
+        send_mail(
+            'From Demo',  
+            'This email from django simple message service', 
+            settings.AWS_SES_FROM_EMAIL, 
+            ['@email.com'], 
+        )
+        return Response({"message": "Email sent successfully!"})
